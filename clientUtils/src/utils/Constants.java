404370d3d09165e055969356ee73db62b454b7b7
Liron aviv Kotev <lironkt@mta.ac.il>
2022-09-25 18:52:03 +0300

    init

--- clientUtils/src/utils/Constants.java
+++ clientUtils/src/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public final static String BASE_DOMAIN = "localhost";
     public final static String BASE_URL = "http://" + BASE_DOMAIN + ":8080";// http://localhost:8080
     public final static String CONTEXT_PATH = "/app";
-    public final static String FULL_SERVER_PATH = BASE_URL + CONTEXT_PATH;// http://localhost:8080/app
-    public final static String LOGIN_PAGE = FULL_SERVER_PATH + "/LoginServlet";// http://localhost:8080/app/LoginServlet
+    public final static String FULL_SERVER_PATH = BASE_URL + CONTEXT_PATH;// http://localhost:8080/Application_Web_exploded
+    public final static String LOGIN_PAGE = FULL_SERVER_PATH + "/LoginServlet";// http://localhost:8080/Application_Web_exploded/LoginServlet
 
 }
