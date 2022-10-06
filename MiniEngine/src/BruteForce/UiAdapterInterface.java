919f1a2850dc082af1885ef4cc9524d8b5e45852
chenpakman <chen36361@gmail.com>
2022-10-07 02:25:23 +0300

    agentResults

--- MiniEngine/src/BruteForce/UiAdapterInterface.java
+++ MiniEngine/src/BruteForce/UiAdapterInterface.java
@@ -7,8 +7,8 @@ import java.util.concurrent.BlockingQueue;
 public interface UiAdapterInterface {
 
         /*public void updateServer(List<BruteForceResultDTO> bruteForceResultDTO);*/
-        public void updateResults(BlockingQueue<BruteForceResultDTO> bruteForceResultDTO);
-        public void updateResultsOnAgent(BlockingQueue<BruteForceResultDTO> bruteForceResultDTO);
+        public void saveResultsOnServer(BlockingQueue<BruteForceResultDTO> bruteForceResultDTO);
+   /*     public void updateResultsOnAgent(BlockingQueue<BruteForceResultDTO> bruteForceResultDTO);*/
 
 
 
