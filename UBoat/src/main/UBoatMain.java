9b5b487597973f6d85c7ce98cee312db3862b928
chenpakman <chen36361@gmail.com>
2022-10-16 19:44:25 +0300

    fixes

--- UBoat/src/main/UBoatMain.java
+++ UBoat/src/main/UBoatMain.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -21,6 +22,7 @@ public class UBoatMain extends Application {
             //uBoatLoginController.setMediator(mediator);
             uBoatLoginController.setPrimaryStageAndLoadInTheBackgroundSuperScreen(primaryStage);
             primaryStage.setTitle("UBoat");
+            primaryStage.getIcons().add(new Image("/Resources/uboatIcon.jpg"));
             Scene scene = new Scene(root);
             primaryStage.setMinHeight(300f);
             primaryStage.setMinWidth(400f);
